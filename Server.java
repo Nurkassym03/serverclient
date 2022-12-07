26faeee879068b389ef3a2024317c91fb1713156
Nurkassym03 <119846588+Nurkassym03@users.noreply.github.com>
2022-12-07 18:36:12 +0600

    Add files via upload

--- Server.java
+++ Server.java
@@ -1,9 +1,6 @@
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.net.*;
 import java.lang.ClassNotFoundException;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 
 public class Server {
